e159c5daf6edfe1e1d28a8abdf201500f5edc050
marioquixari <104331767+marioquixari@users.noreply.github.com>
2022-08-08 05:10:54 +0100

    Simple Clock using Html/Css and JavaScript

---
+++ script.js
@@ -0,0 +1,18 @@
+const horas = document.getElementById('horas')
+const minutos = document.getElementById('minutos')
+const segundos = document.getElementById('segundos')
+
+const relogio = setInterval(function time(){
+    let data = new Date()
+    hr = data.getHours()
+    min = data.getMinutes()
+    sec = data.getSeconds()
+
+    if (hr < 10) hr = '0' + hr
+    if (min < 10) min = '0' + min
+    if (sec <10) sec = '0' + sec
+
+    horas.textContent = hr
+    minutos.textContent = min
+    segundos.textContent = sec
+})
